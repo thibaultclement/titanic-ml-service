fc0d57a42e0c535daff34d03cdcb787ea894c4cc
thib <thibault.clement@gmail.com>
2026-06-11 20:09:00 +0200

    feat(predict): add function for predict step

---
+++ src/titanic/predict.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def make_predictions(model, X_test, passenger_ids=None):
+    predictions = model.predict(X_test).astype(int)
+
+    if passenger_ids is None:
+        index = X_test.index
+    else:
+        index = passenger_ids
+
+    return pd.DataFrame(
+        predictions,
+        index=index,
+        columns=["Survived"]
+    )
+
+
+def save_submission(predictions_df, filepath):
+    predictions_df.to_csv(filepath, index=True)
+    return filepath
